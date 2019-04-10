be9d39f78c8d33e034e11992fb8322117057afa5
Anders Malmgren <anders.malmgren@insclear.se>
2019-04-10 15:30:26 +0200

    Added files

---
+++ FabricApi/FabricApi.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.ServiceFabric.Data;
+
+namespace FabricApi
+{
+    /// <summary>
+    /// The FabricRuntime creates an instance of this class for each service type instance. 
+    /// </summary>
+    internal sealed class FabricApi : StatelessService
+    {
+        public FabricApi(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (like tcp, http) for this service instance.
+        /// </summary>
+        /// <returns>The collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext =>
+                    new KestrelCommunicationListener(serviceContext, "ServiceEndpoint", (url, listener) =>
+                    {
+                        ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting Kestrel on {url}");
+
+                        return new WebHostBuilder()
+                                    .UseKestrel()
+                                    .ConfigureAppConfiguration(builder => builder.AddServiceFabricConfiguration())
+                                    //.UseConfiguration(new ConfigurationBuilder().AddServiceFabricConfiguration().Build())
+                                    .ConfigureServices(
+                                        services => services
+                                            .AddSingleton<StatelessServiceContext>(serviceContext))
+                                    .UseContentRoot(Directory.GetCurrentDirectory())
+                                    .UseStartup<Startup>()
+                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
+                                    .UseUrls(url)
+                                    .Build();
+                    }))
+            };
+        }
+    }
+
+    internal class ServiceFabricConfigurationProvider : ConfigurationProvider
+    {
+
+        public override void Load()
+        {
+            Data["Logging:LogLevel:Default"] = "Information";
+            Data["Logging:Debug:LogLevel:Default"] = "Information";
+            
+        }
+    }
+
+    internal class ServiceFabricConfigurationSource : IConfigurationSource
+    {
+
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new ServiceFabricConfigurationProvider();
+        }
+    }
+
+    public static class ServiceFabricConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddServiceFabricConfiguration(this IConfigurationBuilder builder)
+        {
+            builder.Add(new ServiceFabricConfigurationSource());
+            return builder;
+        }
+    }
+}
