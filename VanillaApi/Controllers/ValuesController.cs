5cebb52258555fcfa309f0dbb195836032946db5
Anders Malmgren <anders.malmgren@outlook.com>
2019-04-23 19:42:30 +0200

    Added working vanilla asp core example

---
+++ VanillaApi/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace VanillaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+        private readonly IConfiguration _config;
+
+        public ValuesController(ILogger<ValuesController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            _logger.LogInformation("Test logging");
+
+            return new[] { $"LogLevel in config: {_config["Logging:Debug:LogLevel:Default"]}" }.Union(Enum.GetValues(typeof(LogLevel)).Cast<LogLevel>().Select(ll => $"LogLevel '{ll}': {_logger.IsEnabled(ll)}")).ToList();
+
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ VanillaApi/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace VanillaApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder => builder.AddTestConfiguration()) //Normally this is done through .json config. But douing it this way to prove it works when done through vanilla pipeline not service fabric
+                .UseStartup<Startup>();
+    }
+
+    internal class VanillaCoreConfigurationProvider : ConfigurationProvider
+    {
+
+        public override void Load()
+        {
+            Data["Logging:LogLevel:Default"] = "Information";
+            Data["Logging:Debug:LogLevel:Default"] = "Information";
+
+        }
+    }
+
+    internal class VanillaCoreConfigurationSource : IConfigurationSource
+    {
+
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new VanillaCoreConfigurationProvider();
+        }
+    }
+
+    public static class VanillaCoreConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddTestConfiguration(this IConfigurationBuilder builder)
+        {
+            builder.Add(new VanillaCoreConfigurationSource());
+            return builder;
+        }
+    }
+}
